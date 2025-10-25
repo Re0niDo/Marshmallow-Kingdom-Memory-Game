de83d026a50bbd609c821a07578a20effa4c54b4
Leo <239386316+Re0niDo@users.noreply.github.com>
2025-10-25 18:04:44 +0300

    refactor: Consistent formatting

--- src/main.js
+++ src/main.js
@@ -1,24 +1,21 @@
-import { Preloader } from './Preloader';
-import { Play } from './Play';
-import Phaser from 'phaser';
-import { COLORS } from './colors';
+import { Preloader } from "./Preloader";
+import { Play } from "./Play";
+import Phaser from "phaser";
+import { COLORS } from "./colors";
 
 const config = {
-    title: 'Marshmallow Kingdom Memory Game',
-    type: Phaser.AUTO,
-    width: 549,
-    height: 480,
-    parent: 'game-container',
-    backgroundColor: COLORS.BACKGROUND,
-    pixelArt: true,
-    scale: {
-        mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH
-    },
-    scene: [
-        Preloader,
-        Play
-    ]
+  title: "Marshmallow Kingdom Memory Game",
+  type: Phaser.AUTO,
+  width: 549,
+  height: 480,
+  parent: "game-container",
+  backgroundColor: COLORS.BACKGROUND,
+  pixelArt: true,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+  scene: [Preloader, Play],
 };
 
 new Phaser.Game(config);
